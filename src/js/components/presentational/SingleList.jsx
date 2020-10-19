f9614d426e736ceb4d881a1d1d34323f23e4363b
Dylan Connor <DylanJConnor4@gmail.com>
2020-10-18 21:14:50 -0700

    show list with correct location

--- src/js/components/presentational/SingleList.jsx
+++ src/js/components/presentational/SingleList.jsx
@@ -2,25 +2,25 @@ import React, { Component } from 'react';
 import { Search } from "../presentational/Search.jsx";
 
 export class SingleList extends Component {
-  
+
   constructor(props) {
     super(props);
     // this.handleChange = this.handleChange.bind(this);
     this.state = {
-      list: ''
+      list: '',
+      title: ''
     }
-    
   }
 
-  
+
 
   showList = async () => {
     let id = this.props.listId;
-    console.log('id',id);
+    console.log('id', id);
     let url = 'http://localhost:3000/' + id;
     const call = await fetch(url);
     const data = await call.json();
-    console.log('single list',data);
+    console.log('single list', data);
     this.setState({
       list: data,
       added: false
@@ -29,37 +29,22 @@ export class SingleList extends Component {
   }
 
   addToList = async (e) => {
-    // post request to add to a list
     e.preventDefault();
-    
 
-    // let list = this.state.list;
     let item = e.currentTarget.querySelector('.product-info:nth-of-type(2) p:nth-of-type(2)').innerText;
     let aisle = e.currentTarget.querySelector('.product-info:nth-of-type(3) p:nth-of-type(2)').innerText;
     let listItem = {};
     listItem.item = item;
     listItem.aisle = aisle;
-    // list.push(listItem);
-    // console.log(list)
-    // this.setState({
-    //   list: list
-    // })
-    // let form = document.querySelector('')
-    // let data = 
-    console.log(e.currentTarget)
     let id = e.currentTarget.dataset.listid;
     console.log(listItem)
-    let url = 'http://localhost:3000/' + id + '?_method=PUT';
+    let url = 'http://localhost:3000/' + id + '/item?_method=PUT';
     const call = await fetch(url, {
       method: 'POST',
       headers: {
         "Content-Type": "application/json"
       },
-      // headers: {
-      //   "Content-Type": "application/x-www-form-urlencoded"
-      // },
-      // body: new FormData(e.currentTarget)
-      body: JSON.stringify({list:listItem})
+      body: JSON.stringify({ list: listItem })
     });
     const data = await call.json();
     console.log('updated list', data);
@@ -67,49 +52,97 @@ export class SingleList extends Component {
       added: true
     });
   }
-  
-  componentDidUpdate(){
+
+  handleBlur = async (e) => {
+    // save title
+    let title = e.target.value;
+    let id = e.target.id;
+    let url = 'http://localhost:3000/' + id + '/title?_method=PUT';
+    const call = await fetch(url, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ title: title })
+    });
+    const data = await call.json();
+    console.log('updated title', data);
   }
 
-  componentDidMount () {
-    // console.log('mount');
+  handleChange = e => {
+
+    this.setState({title: e.target.value});
+  }
+
+  handleFocus = e => {
+
+    this.setState({title: ''});
+  }
+
+
+  componentDidMount() {
     this.showList();
-    
   }
 
-render() {
-  let handleExistence = (item) => {
-    return item ? item : 'N/A';
-  }
-  let list = this.state.list;
-  let listId = this.props.listId
-  let addToList = this.props.addToList;
-  if (!list) {
-    return <div className="spinner">Loading List...<span></span></div>;
-  }
+  render() {
+    let list = this.state.list;
+    let title = this.state.title;
+    let listId = this.props.listId
+    let addToList = this.props.addToList;
+    let newList = this.props.newList;
+    list.list && list.list.sort((a, b) => a.aisle.split(':')[1] - b.aisle.split(':')[1])
+    if (!list) {
+      return <div className="spinner">Loading List...<span></span></div>;
+    }
 
-  let date = new Date(list.created)
+    let date = new Date(newList ? newList.created : list.created)
     return (
       <div className="list-wrapper">
-        <p>{list.title}</p>
-        <p>Created: {date.toDateString()}</p>
+        {addToList ? (
+          <p>Editing {list.title}</p>
+        ) : (
+            <div className="list-header">
+              <input
+                id={newList ? newList._id : listId}
+                type="text"
+                onFocus={this.handleFocus}
+                onBlur={this.handleBlur}
+                value={title}
+                onChange={this.handleChange}
+                placeholder={newList ? newList.title : (title != '' ? title : list.title)}
+              />
+
+              <p>{date.toDateString()}</p>
+            </div>
+          )}
+
         {addToList ? (
           <Search
-              listId={listId}
-              list={list}
-              addToList={this.addToList}
-            />
+            locationId={list.locationId}
+            listId={listId}
+            list={list}
+            addToList={this.addToList}
+            showList={this.showList}
+          />
         ) : (
-      <ul className="list">
-        {/* {console.log(list.list)} */}
-        {list.list.length > 0 ? list.list.map((item,i) => (
-        
-          <li key={i} className="item"><span>{item.item}</span><span>{item.aisle}</span></li>
-        )) : (
-          <p>No items in your list</p>
-        )}
-      </ul>
-        )}
+            <ul className="list">
+              <li className="item">
+                <span>Item</span>
+                <span>Aisle Number</span>
+              </li>
+              {list.list && list.list.length > 0 ? list.list.map((item, i) => {
+                let splitAisleText = item.aisle.split(':');
+                return (
+                  <li key={i} className="item">
+                    <span>{item.item}</span>
+                    <span>{splitAisleText[1]}</span>
+                  </li>
+                );
+              }) : (
+                  <p>No items in your list</p>
+                )}
+            </ul>
+          )}
       </div>
 
     );
