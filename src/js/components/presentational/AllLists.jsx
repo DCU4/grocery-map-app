65f8a3869807938f5be5f9af77fb10f58adbb37a
Dylan Connor <DylanJConnor4@gmail.com>
2020-10-18 21:14:29 -0700

    update aisle ui

--- src/js/components/presentational/AllLists.jsx
+++ src/js/components/presentational/AllLists.jsx
@@ -2,63 +2,59 @@ import React, { Component } from 'react';
 import { SingleList } from "./SingleList.jsx";
 
 export class AllLists extends Component {
-  
+
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
     this.state = {
       lists: '',
     }
-    
+
   }
 
   getYourLists = async () => {
     const call = await fetch(`http://localhost:3000/`);
     const data = await call.json();
-    console.log('your list',data);
+    console.log('your list', data);
     this.setState({
       lists: data
     });
   }
 
-
-  
-
-  handleChange =  () =>{
+  handleChange = () => {
     // this.props.onClick();
   }
 
-  showSingleList = () => {
-    // on list click 
-  }
 
-
-  
-  componentDidUpdate(){
-  }
-
-  componentDidMount () {
+  componentDidMount() {
     this.getYourLists();
   }
 
-render() {
-  let handleExistence = (item) => {
-    return item ? item : 'N/A';
-  }
-  let lists = this.state.lists;
-  // lists.sort();
+  render() {
+    let lists = this.state.lists;
+    lists && lists.lists.sort((a, b) => new Date(b.created) - new Date(a.created));
+    let truncate = (input) => input.length > 8 ? `${input.substring(0, 8)}...` : input;
+    if (!lists) {
+      return <div className="spinner">Loading Your Lists...<span></span></div>;
+    }
     return (
       <div className="your-list-wrapper">
-        
+
         <ul className="your-list">
-          {/* needs ID from single list */}
-        {lists ? lists.lists.map((list,i) => (
-          <li onClick={this.props.showSingleNote} id={list._id} key={i} className="list">{list.title}</li>
-          
-        )) : (
-          <p>No lists... yet</p>
-        )}
-       </ul>
+          {lists ? lists.lists.map((list, i) => {
+
+            let date = new Date(list.created);
+            return (
+              <li onClick={this.props.showSingleNote} id={list._id} key={i} className="list">
+                <span>{truncate(list.title)}</span>
+                <span>{date.toDateString()}</span>
+                {/* <span>Store</span> */}
+              </li>)
+
+          }) : (
+              <p>No lists... yet</p>
+            )}
+        </ul>
       </div>
 
     );
