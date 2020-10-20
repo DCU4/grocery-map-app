37b371ddc0b19655adc1c49c6c5caceac37b6016
Dylan Connor <DylanJConnor4@gmail.com>
2020-10-19 22:04:02 -0700

    fix date

--- src/js/components/presentational/AllLists.jsx
+++ src/js/components/presentational/AllLists.jsx
@@ -33,7 +33,7 @@ export class AllLists extends Component {
   render() {
     let lists = this.state.lists;
     lists && lists.lists.sort((a, b) => new Date(b.created) - new Date(a.created));
-    let truncate = (input) => input.length > 8 ? `${input.substring(0, 8)}...` : input;
+    let truncate = (input) => input.length > 15 ? `${input.substring(0, 15)}...` : input;
     if (!lists) {
       return <div className="spinner">Loading Your Lists...<span></span></div>;
     }
@@ -44,10 +44,14 @@ export class AllLists extends Component {
           {lists ? lists.lists.map((list, i) => {
 
             let date = new Date(list.created);
+            let year = date.getFullYear();
+            let month = date.getMonth();
+            let day = date.getDate();
+
             return (
               <li onClick={this.props.showSingleNote} id={list._id} key={i} className="list">
                 <span>{truncate(list.title)}</span>
-                <span>{date.toDateString()}</span>
+                <span>{month+1}/{day}/{year}</span>
                 {/* <span>Store</span> */}
               </li>)
 
