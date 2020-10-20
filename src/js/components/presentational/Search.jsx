6b84a0dfdb747d8599fc30525f5a64eb1cf000b8
Dylan Connor <DylanJConnor4@gmail.com>
2020-10-19 22:03:50 -0700

    check if mounted

--- src/js/components/presentational/Search.jsx
+++ src/js/components/presentational/Search.jsx
@@ -2,31 +2,31 @@ import React, { Component } from 'react';
 
 
 export class Search extends Component {
-  
+  _isMounted = false;
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
     this.state = {
       search: '',
-      data: '',
+      data: null,
       searching: false,
       listId: '',
       location: false
     }
-    
+
   }
 
 
   // to check search results based on the list you are adding to
   getSingleListData = () => {
     let listId = this.props.listId;
-    
+
   }
 
   // favorite items
 
   // previously added items 
-  
+
   getLocation = async () => {
     let locationId = this.props.locationId;
     const call = await fetch(`http://localhost:3000/get-single-location`, {
@@ -34,9 +34,10 @@ export class Search extends Component {
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({locationId: locationId})
+      body: JSON.stringify({ locationId: locationId })
     });
     const data = await call.json();
+    this._isMounted && 
     this.setState({
       location: data
     });
@@ -48,7 +49,6 @@ export class Search extends Component {
     });
     let search = this.state.search;
     let locationId = this.props.locationId;
-    console.log(search, locationId);
     event.preventDefault();
     const call = await fetch(`http://localhost:3000/get-data`, {
       method: 'POST',
@@ -61,7 +61,8 @@ export class Search extends Component {
       })
     });
     const data = await call.json();
-    console.log('products',data)
+    console.log('products', data)
+    this._isMounted && 
     this.setState({
       data: data,
       searching: false
@@ -70,14 +71,16 @@ export class Search extends Component {
 
 
   handleChange = event => {
-    this.setState({search: event.target.value});
+    this.setState({ search: event.target.value });
   }
 
   componentDidMount() {
+    this._isMounted = true;
     this.getLocation();
   }
-  
-  componentWillUnmount () {
+
+  componentWillUnmount() {
+    this._isMounted = false;
     this.props.showList();
   }
 
@@ -90,75 +93,84 @@ export class Search extends Component {
     let data = this.state.data;
     let searching = this.state.searching;
     let location = this.state.location;
-    console.log('search list',data);
     if (searching) {
       return <div className="spinner">Loading Data...<span></span></div>;
     }
-    if(!location) {
-
+    if (!location && !location.data) {
       return <div className="spinner">Loading Location...<span></span></div>;
     }
+
+    // console.log('search list', data.data);
     return (
       <div className="search-list">
-        <form id="search" onSubmit={this.getData}>
-        
+        <form className="fade-in" id="search" onSubmit={this.getData}>
+
           <label htmlFor="search">Search {location.data.chain} in {location.data.address.city}, {location.data.address.state}</label>
           <input value={this.state.value} type="search" name="search" onChange={this.handleChange} />
         </form>
-        {data && data.data && data.data.map((d,i)=> {
-          let found = list.list.some(l => l.item == d.description)
+        {data && data.data.length > 0 ?
 
-          return (
-        <div className="product-wrapper" key={i} >
-        <p>Search Results</p>
-          <form data-listid={listId}  className="product" onSubmit={this.props.addToList}>
-            <input type="hidden" name="item" value={d.description} />
-            <input type="hidden" name="aisle" value={d.aisleLocations[0] ? d.aisleLocations[0].number : ''} />
-            <div className="product-info-wrapper">
-              <div className="product-info">
-                <p>Brand:</p>
-                <p>{d.brand}</p>
-              </div>
-              <div className="product-info">
-                <p>Product:</p>
-                <p>{handleExistence(d.description)}</p>
-              </div>
+          data.data.map((d, i) => {
+            let found = list.list.some(l => l.item == d.description)
 
-              {d.aisleLocations[0] ? 
-              <div className="product-info">
-                <p>Location: {handleExistence(d.aisleLocations[0].description)}</p>
-                <p>Aisle Number: {handleExistence(d.aisleLocations[0].number)}</p>
-                <p>Shelf Number: {handleExistence(d.aisleLocations[0].shelfNumber)}</p>
-              </div>
-              :
-              <div className="product-info">
-                <p>no aisle info</p>
-              </div>
-              }
-              {found ? (
+              return (
+                <div className="product-wrapper fade-in" key={i} >
+                  <p>Search Results</p>
+                  <form data-listid={listId} className="product" onSubmit={this.props.addToList}>
+                    <input type="hidden" name="item" value={d.description} />
+                    <input type="hidden" name="aisle" value={d.aisleLocations[0] ? d.aisleLocations[0].number : ''} />
+                    <div className="product-info-wrapper ">
+                      <div className="product-info fade-in">
+                        <p>Brand:</p>
+                        <p>{d.brand}</p>
+                      </div>
+                      <div className="product-info fade-in">
+                        <p>Product:</p>
+                        <p>{handleExistence(d.description)}</p>
+                      </div>
 
-                <strong className="add-item">On My List</strong>
-              ) : (
+                      {d.aisleLocations[0] ?
+                        <div className="product-info fade-in">
+                          <p>Location: {handleExistence(d.aisleLocations[0].description)}</p>
+                          <p>Aisle Number: {handleExistence(d.aisleLocations[0].number)}</p>
+                          <p>Shelf Number: {handleExistence(d.aisleLocations[0].shelfNumber)}</p>
+                        </div>
+                        :
+                        <div className="product-info fade-in">
+                          <p>no aisle info</p>
+                        </div>
+                      }
+                      {found ? (
 
-                <button><strong className="add-item">Add To List +</strong></button>
-              )}
+                        <strong className="add-item fade-in">On My List</strong>
+                      ) : (
+
+                          <button><strong className="add-item fade-in">Add To List +</strong></button>
+                        )}
+                    </div>
+
+                    <div className="product-image">
+                      {d.images.map((img, i) => {
+                        let src = img.sizes.filter(size => (size.size == 'xlarge'));
+                        return img.featured && src[0] &&
+                          <img key={i} src={src[0].url} />
+
+                      })}
+                    </div>
+
+                  </form>
+                </div>
+              );
+          }
+          ) : (
+            data &&
+            <div className="product-info ">
+              <p>Sorry no products</p>
             </div>
+          )}
 
-            <div className="product-image">
-            {d.images.map((img,i)=> {
-                let src = img.sizes.filter(size => (size.size == 'xlarge'));
-                  return img.featured && src[0] &&
-                  <img key={i} src={src[0].url } />
-                  
-              })}
-            </div>
-
-          </form>
-          </div>
-          );
-        })}
       </div>
-      
+
     );
   }
 }
